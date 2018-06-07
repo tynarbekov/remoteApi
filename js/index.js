9fff7399159b91cee17a9caf571fb3c4e7f88607
marat <tynarbekov95@gmail.com>
2018-06-08 00:41:11 +0600

    adding search

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 var postList = [];
 var authorList = [];
-var allData = [];
+var storedPosts;
 
 function ajaxGet(url) {
   return new Promise(function(resolve, reject) {
@@ -60,18 +60,71 @@ function callAjax(){
           postList[i].userInfo = authors[j]
         }
       }
-      title = "Title: ".bold() + "<a href='post.html?postId=" + posts[i].id + "'>" + posts[i].title + "</a>" + ", ";
-      author = "Author: ".bold() + posts[i].userInfo.name;
-      document.getElementById("postList").innerHTML+= title + author + "<br>";
+    }
+    toStorage();
+    outputHtml();
+  }
+
+  function toStorage(){
+    localStorage.setItem("posts",JSON.stringify(postList))
+    storedPosts = JSON.parse(localStorage.getItem("posts"))
+  }
+
+  function outputHtml(){
+    for (var i = 0; i < storedPosts.length; i++) {
+      title = "Title: ".bold() + "<a href='post.html?postId=" + storedPosts[i].id + "'>" + storedPosts[i].title + "</a>";
+      author = "Author: ".bold() + storedPosts[i].userInfo.name;
+      document.getElementById("postList").innerHTML+= title + "<br>"   + author + "<br>" + "---" + "<br>";
     }
   }
 
-  //
-  function setUrl(url){
-    console.log(postList);
-    // url = url.substr(url.length -1);
-    // return postList[url];
+  function getPostInfo(url){
+    url = url.split("=").pop();
+    url = parseInt(url);
+    var postInfo = JSON.parse(localStorage.getItem("posts"));
+    var post = postInfo.find( post => post.id === url );
+    outputPostInfo(post);
   }
+
+  function outputPostInfo(post){
+    title = "Title: " + post.title;
+    body = "Body: " + post.body;
+    author = "Author: " + post.userInfo.name;
+    username = "Username: " + post.userInfo.username;
+    email = "Email: " + post.userInfo.email;
+    address = "Address: Street: " + post.userInfo.address.street + ", suite: "
+    + post.userInfo.address.suite +
+    ", city: " + post.userInfo.address.street +
+    ", zipcode: " + post.userInfo.address.zipcode +
+    ", geo: " + "lat: " + post.userInfo.address.geo.lat + ", lng: " + post.userInfo.address.geo.lng;
+    phone = "Phone: " + post.userInfo.phone;
+    website = "Website: " + post.userInfo.website;
+    company = "Company Name: " + post.userInfo.company.name + ", catchPhrase: " + post.userInfo.company.catchPhrase + ", bs: " + post.userInfo.company.bs;
+
+    document.getElementById("postTitle").innerHTML+= title;
+    document.getElementById("postBody").innerHTML+= body;
+    document.getElementById("authorName").innerHTML+= author;
+    document.getElementById("username").innerHTML+= username;
+    document.getElementById("email").innerHTML+= email;
+    document.getElementById("address").innerHTML+= address;
+    document.getElementById("phone").innerHTML+= phone;
+    document.getElementById("website").innerHTML+= website;
+    document.getElementById("company").innerHTML+= company;
+
+  }
+
+
+  function search(by){
+    
+  }
+
+
+
+
+
+
+
+
 
 
   //
